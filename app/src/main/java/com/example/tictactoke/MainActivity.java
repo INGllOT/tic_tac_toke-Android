28d771f68839b747790536959805ff18e34bd2e8
Dariusz Nowak <dariusz.nowak012@gmail.com>
2023-04-01 22:32:50 +0200

    save work

--- app/src/main/java/com/example/tictactoke/MainActivity.java
+++ app/src/main/java/com/example/tictactoke/MainActivity.java
@@ -8,17 +8,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 
 //import android.support.v7.app.AppCompatActivity;
-import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main3);
 
 
 
